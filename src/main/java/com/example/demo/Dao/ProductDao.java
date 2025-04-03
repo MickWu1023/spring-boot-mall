21872c37a30d40adc857e9a99a5543e471840b5b
WUMINCHE3331 <wuminzhe9@gamil.com>
2025-04-04 00:08:54 +0800

    查詢所有產品

--- src/main/java/com/example/demo/Dao/ProductDao.java
+++ src/main/java/com/example/demo/Dao/ProductDao.java
@@ -3,8 +3,10 @@ package com.example.demo.Dao;
 import com.example.demo.Model.Product;
 import com.example.demo.dto.ProductRequest;
 
-public interface ProductDao {
+import java.util.List;
 
+public interface ProductDao {
+    List<Product> getProducts();
     Product getProductById(Integer productId);
     Integer createProduct(ProductRequest productRequest);
     void updateProduct(Integer productId,ProductRequest productRequest);
--- src/main/java/com/example/demo/Service/ProductService.java
+++ src/main/java/com/example/demo/Service/ProductService.java
@@ -3,10 +3,14 @@ package com.example.demo.Service;
 import com.example.demo.Model.Product;
 import com.example.demo.dto.ProductRequest;
 
+import java.util.List;
+
 public interface ProductService {
+    List<Product> getProducts();
     Product getProductById(Integer productId);
     Integer createProduct (ProductRequest productRequest);
     void updateProduct (Integer productId,ProductRequest productRequest);
     void deleteProductById (Integer productId);
+
 
 }
